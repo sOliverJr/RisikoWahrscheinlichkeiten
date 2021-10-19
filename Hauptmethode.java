585069849237225ac56406582834d939f615f08d
sOliverJr <64847610+sOliverJr@users.noreply.github.com>
2021-10-19 17:14:35 +0200

    Version 1.0
    Alle verschiedenen Würfel-Kombinationen sind Implementiert und werden in der Form von Lesbaren Tabellen zur Laufzeit ausgegeben. Es wird außerdem sowohl eine Tabelle für die Sicht des Angreifers als auch eine für die Sicht des Verteidigers ausgegeben.

--- Hauptmethode.java
+++ Hauptmethode.java
@@ -1,10 +1,49 @@
 public class Hauptmethode {
     public static void main(String[] args) {
-        Risiko risiko = new Risiko();
-        double[] werte = risiko.rechnen();
-        System.out.println("Die Wahrscheinlichkeit zu gewinnen ist: " + (werte[0] / werte[3]));
-        System.out.println("Die Wahrscheinlichkeit zu verlieren ist: " + (werte[2] / werte[3]));
-        System.out.println("Die Wahrscheinlichkeit zu Unentschieden ist: " + (werte[1] / werte[3]));
-        System.out.println("Die insgesamte Wahrscheinlichkeit ist: " + ((werte[0] / werte[3]) + (werte[1] / werte[3]) + (werte[2] / werte[3])));
+        System.out.println();
+
+        // Aus Sicht von einem Angreifer
+        System.out.println("Aus der Sicht des Angreifers:");
+        System.out.println("Anzahl Angreifer:       Anzahl Verteidiger:         W(Sieg):        W(Verlust):       W(Unentschieden): "); 
+        Risiko risiko1 = new Risiko();
+        double[] werte1 = risiko1.rechnen_eins_eins();
+        System.out.println("1                       1                           " + Math.round(werte1[1]*100.0)/100.0 + "            " + Math.round(werte1[0]*100.0)/100.0 + "              -");
+
+        Risiko risiko2 = new Risiko();
+        double[] werte2 = risiko2.rechnen_eins_zwei();
+        System.out.println("1                       2                           " + Math.round(werte2[1]*100.0)/100.0 + "            " + Math.round(werte2[0]*100.0)/100.0 + "              -");
+
+        Risiko risiko3 = new Risiko();
+        double[] werte3 = risiko3.rechnen_zwei_eins();
+        System.out.println("2                       1                           " + Math.round(werte3[1]*100.0)/100.0 + "            " + Math.round(werte3[0]*100.0)/100.0 + "              -");
+
+        Risiko risiko4 = new Risiko();
+        double[] werte4 = risiko4.rechnen_zwei_zwei();
+        System.out.println("2                       2                           " + Math.round(werte4[1]*100.0)/100.0 + "            " + Math.round(werte4[0]*100.0)/100.0 + "              " + Math.round(werte4[2]*100.0)/100.0);
+
+        Risiko risiko5 = new Risiko();
+        double[] werte5 = risiko5.rechnen_drei_eins();
+        System.out.println("3                       1                           " + Math.round(werte5[1]*100.0)/100.0 + "            " + Math.round(werte5[0]*100.0)/100.0 + "              -");
+
+        Risiko risiko6 = new Risiko();
+        double[] werte6 = risiko6.rechnen_drei_zwei();
+        System.out.println("3                       2                           " + Math.round(werte6[1]*100.0)/100.0 + "            " + Math.round(werte6[0]*100.0)/100.0 + "              " + Math.round(werte6[2]*100.0)/100.0);
+
+        
+        System.out.println();
+        System.out.println();
+        System.out.println();
+
+
+        // Aus Sicht von einem Verteidiger
+        System.out.println("Aus der Sicht des Verteidigers:");
+        System.out.println("Anzahl Angreifer:       Anzahl Verteidiger:         W(Sieg):        W(Verlust):       W(Unentschieden): "); 
+        System.out.println("1                       1                           " + Math.round(werte1[0]*100.0)/100.0 + "            " + Math.round(werte1[1]*100.0)/100.0 + "              -");
+        System.out.println("1                       2                           " + Math.round(werte2[0]*100.0)/100.0 + "            " + Math.round(werte2[1]*100.0)/100.0 + "              -");
+        System.out.println("2                       1                           " + Math.round(werte3[0]*100.0)/100.0 + "            " + Math.round(werte3[1]*100.0)/100.0 + "              -");
+        System.out.println("2                       2                           " + Math.round(werte4[0]*100.0)/100.0 + "            " + Math.round(werte4[1]*100.0)/100.0 + "              " + Math.round(werte4[2]*100.0)/100.0);
+        System.out.println("3                       1                           " + Math.round(werte5[0]*100.0)/100.0 + "            " + Math.round(werte5[1]*100.0)/100.0 + "              -");
+        System.out.println("3                       2                           " + Math.round(werte6[0]*100.0)/100.0 + "            " + Math.round(werte6[1]*100.0)/100.0 + "              " + Math.round(werte6[2]*100.0)/100.0);
+
     }
 }
--- Risiko.java
+++ Risiko.java
@@ -13,7 +13,202 @@ public class Risiko{
     int bester_würfel_angreifer, zweitbester_würfel_angreifer;
     int bester_würfel_verteidiger, zweitbester_würfel_verteidiger;
 
-    public double[] rechnen(){
+    public double[] rechnen_eins_eins(){
+        while (würfel_angreifer_1 < 7) {
+            while(würfel_verteidiger_1 < 7 ){
+                bester_würfel_angreifer = würfel_angreifer_1;
+
+                bester_würfel_verteidiger = würfel_verteidiger_1;
+
+                if (bester_würfel_verteidiger >= bester_würfel_angreifer){
+                    anzahl_siege ++;
+                }else {
+                    anzahl_verluste ++;
+                }
+                anzahl_gesamt ++;
+                würfel_verteidiger_1 ++;
+            }
+            würfel_verteidiger_1 = 1;
+            würfel_angreifer_1 ++;
+        }
+
+        wahrsch_sieg = (double)anzahl_siege / (double)anzahl_gesamt;
+        wahrsch_verlust = (double)anzahl_verluste / (double)anzahl_gesamt;
+
+        return new double[]{wahrsch_sieg, wahrsch_verlust};
+    }
+
+    public double[] rechnen_eins_zwei(){
+        while (würfel_angreifer_1 < 7) {
+            while(würfel_verteidiger_1 < 7 ){
+                while(würfel_verteidiger_2 < 7 ){
+                    bester_würfel_angreifer = würfel_angreifer_1;
+
+                    if (würfel_verteidiger_1 > würfel_verteidiger_2){
+                        bester_würfel_verteidiger = würfel_verteidiger_1;
+                    }else {
+                        bester_würfel_verteidiger = würfel_verteidiger_2;
+                    }
+
+                    if (bester_würfel_verteidiger >= bester_würfel_angreifer){
+                        anzahl_siege ++;
+                    }else {
+                        anzahl_verluste ++;
+                    }
+                    anzahl_gesamt ++;
+                    würfel_verteidiger_2 ++;
+                }
+                würfel_verteidiger_2 = 1;
+                würfel_verteidiger_1 ++;
+            }
+            würfel_verteidiger_1 = 1;
+            würfel_angreifer_1 ++;
+        }
+
+        wahrsch_sieg = (double)anzahl_siege / (double)anzahl_gesamt;
+        wahrsch_verlust = (double)anzahl_verluste / (double)anzahl_gesamt;
+
+        return new double[]{wahrsch_sieg, wahrsch_verlust};
+    }
+
+    public double[] rechnen_zwei_eins(){
+        while (würfel_angreifer_1 < 7) {
+            while (würfel_angreifer_2 < 7) {
+                while(würfel_verteidiger_1 < 7 ){
+                    if (würfel_angreifer_1 > würfel_angreifer_2){
+                        bester_würfel_angreifer = würfel_angreifer_1;
+                    }else {
+                        bester_würfel_angreifer = würfel_angreifer_2;
+                    }
+
+                    bester_würfel_verteidiger = würfel_verteidiger_1;
+
+                    if (bester_würfel_verteidiger >= bester_würfel_angreifer ){
+                        anzahl_siege ++;
+                    }else{
+                        anzahl_verluste ++;
+                    }
+
+                    anzahl_gesamt ++;
+                    würfel_verteidiger_1 ++;
+                }
+                würfel_verteidiger_1 = 1;
+                würfel_angreifer_2 ++;
+            }
+            würfel_angreifer_2 = 1;
+            würfel_angreifer_1 ++;
+        }
+
+        wahrsch_sieg = (double)anzahl_siege / (double)anzahl_gesamt;
+        wahrsch_verlust = (double)anzahl_verluste / (double)anzahl_gesamt;
+
+        return new double[]{wahrsch_sieg, wahrsch_verlust};
+    }
+
+    public double[] rechnen_zwei_zwei(){
+        while (würfel_angreifer_1 < 7) {
+            while (würfel_angreifer_2 < 7) {
+                while(würfel_verteidiger_1 < 7 ){
+                    while(würfel_verteidiger_2 < 7 ){
+                        if (würfel_angreifer_1 > würfel_angreifer_2){
+                            bester_würfel_angreifer = würfel_angreifer_1;
+                            zweitbester_würfel_angreifer = würfel_angreifer_2;
+                        }else {
+                            bester_würfel_angreifer = würfel_angreifer_2;
+                            zweitbester_würfel_angreifer = würfel_angreifer_1;
+                        }
+
+                        if (würfel_verteidiger_1 > würfel_verteidiger_2){
+                            bester_würfel_verteidiger = würfel_verteidiger_1;
+                            zweitbester_würfel_verteidiger = würfel_verteidiger_2;
+                        }else {
+                            bester_würfel_verteidiger = würfel_verteidiger_2;
+                            zweitbester_würfel_verteidiger = würfel_verteidiger_1;
+                        }
+
+                        if (bester_würfel_verteidiger >= bester_würfel_angreifer && zweitbester_würfel_verteidiger >= zweitbester_würfel_angreifer){
+                            anzahl_siege ++;
+                        }else if(bester_würfel_verteidiger < bester_würfel_angreifer && zweitbester_würfel_verteidiger < zweitbester_würfel_angreifer){
+                            anzahl_verluste ++;
+                        }else if(bester_würfel_verteidiger >= bester_würfel_angreifer && zweitbester_würfel_verteidiger < zweitbester_würfel_angreifer){
+                            anzahl_unentschieden ++;
+                        }else if (bester_würfel_verteidiger < bester_würfel_angreifer && zweitbester_würfel_verteidiger >= zweitbester_würfel_angreifer){
+                            anzahl_unentschieden ++;
+                        }
+                        anzahl_gesamt ++;
+                        würfel_verteidiger_2 ++;
+                    }
+                    würfel_verteidiger_2 = 1;
+                    würfel_verteidiger_1 ++;
+                }
+                würfel_verteidiger_1 = 1;
+                würfel_angreifer_2 ++;
+            }
+            würfel_angreifer_2 = 1;
+            würfel_angreifer_1 ++;
+        }
+
+        wahrsch_sieg = (double)anzahl_siege / (double)anzahl_gesamt;
+        wahrsch_unentschieden = (double)anzahl_unentschieden / (double)anzahl_gesamt;
+        wahrsch_verlust = (double)anzahl_verluste / (double)anzahl_gesamt;
+
+        return new double[]{wahrsch_sieg, wahrsch_verlust, wahrsch_unentschieden};
+    }
+
+    public double[] rechnen_drei_eins(){
+        while (würfel_angreifer_1 < 7) {
+            while (würfel_angreifer_2 < 7) {
+                while (würfel_angreifer_3 < 7) {
+                    while(würfel_verteidiger_1 < 7 ){
+                        if (würfel_angreifer_1 >= würfel_angreifer_2 && würfel_angreifer_1 >= würfel_angreifer_3 && würfel_angreifer_2 >= würfel_angreifer_3){
+                            bester_würfel_angreifer = würfel_angreifer_1;
+                            zweitbester_würfel_angreifer = würfel_angreifer_2;
+                        }else if (würfel_angreifer_1 >= würfel_angreifer_2 && würfel_angreifer_1 >= würfel_angreifer_3 && würfel_angreifer_3 >= würfel_angreifer_2){
+                            bester_würfel_angreifer = würfel_angreifer_1;
+                            zweitbester_würfel_angreifer = würfel_angreifer_3;
+                        }else if (würfel_angreifer_2 >= würfel_angreifer_1 && würfel_angreifer_2 >= würfel_angreifer_3 && würfel_angreifer_1 >= würfel_angreifer_3){
+                            bester_würfel_angreifer = würfel_angreifer_2;
+                            zweitbester_würfel_angreifer = würfel_angreifer_1;
+                        }else if (würfel_angreifer_2 >= würfel_angreifer_1 && würfel_angreifer_2 >= würfel_angreifer_3 && würfel_angreifer_3 >= würfel_angreifer_1){
+                            bester_würfel_angreifer = würfel_angreifer_2;
+                            zweitbester_würfel_angreifer = würfel_angreifer_3;
+                        }else if (würfel_angreifer_3 >= würfel_angreifer_1 && würfel_angreifer_3 >= würfel_angreifer_2 && würfel_angreifer_1 >= würfel_angreifer_2){
+                            bester_würfel_angreifer = würfel_angreifer_3;
+                            zweitbester_würfel_angreifer = würfel_angreifer_1;
+                        }else if (würfel_angreifer_3 >= würfel_angreifer_1 && würfel_angreifer_3 >= würfel_angreifer_2 && würfel_angreifer_2 >= würfel_angreifer_1){
+                            bester_würfel_angreifer = würfel_angreifer_3;
+                            zweitbester_würfel_angreifer = würfel_angreifer_2;
+                        }
+
+                        bester_würfel_verteidiger = würfel_verteidiger_1;
+
+                        if (bester_würfel_verteidiger >= bester_würfel_angreifer){
+                            anzahl_siege ++;
+                        }else {
+                            anzahl_verluste ++;
+                        }
+
+
+                        anzahl_gesamt ++;
+                        würfel_verteidiger_1 ++;
+                    }
+                    würfel_verteidiger_1 = 1;
+                    würfel_angreifer_3 ++;
+                }
+                würfel_angreifer_3 = 1;
+                würfel_angreifer_2 ++;
+            }
+            würfel_angreifer_2 = 1;
+            würfel_angreifer_1 ++;
+        }
+
+        wahrsch_sieg = (double)anzahl_siege / (double)anzahl_gesamt;
+        wahrsch_verlust = (double)anzahl_verluste / (double)anzahl_gesamt;
+
+        return new double[]{wahrsch_sieg, wahrsch_verlust};
+    }
+
+    public double[] rechnen_drei_zwei(){
         while (würfel_angreifer_1 < 7) {
             while (würfel_angreifer_2 < 7) {
                 while (würfel_angreifer_3 < 7) {
@@ -51,11 +246,9 @@ public class Risiko{
                                 anzahl_siege ++;
                             }else if(bester_würfel_verteidiger < bester_würfel_angreifer && zweitbester_würfel_verteidiger < zweitbester_würfel_angreifer){
                                 anzahl_verluste ++;
-                            }else if(bester_würfel_verteidiger >= bester_würfel_angreifer && zweitbester_würfel_verteidiger > zweitbester_würfel_angreifer){
+                            }else if(bester_würfel_verteidiger >= bester_würfel_angreifer && zweitbester_würfel_verteidiger < zweitbester_würfel_angreifer){
                                 anzahl_unentschieden ++;
                             }else if (bester_würfel_verteidiger < bester_würfel_angreifer && zweitbester_würfel_verteidiger >= zweitbester_würfel_angreifer){
-                                anzahl_unentschieden ++;
-                            }else{
                                 anzahl_unentschieden ++;
                             }
 
@@ -76,14 +269,10 @@ public class Risiko{
             würfel_angreifer_1 ++;
         }
 
-        // Aus irgendeinem Grund werden die Werte nicht in die Double-Var. gespeichert
-        wahrsch_sieg = anzahl_siege / anzahl_gesamt;
-        wahrsch_unentschieden = anzahl_unentschieden / anzahl_gesamt;
-        wahrsch_verlust = anzahl_verluste / anzahl_gesamt;
+        wahrsch_sieg = (double)anzahl_siege / (double)anzahl_gesamt;
+        wahrsch_unentschieden = (double)anzahl_unentschieden / (double)anzahl_gesamt;
+        wahrsch_verlust = (double)anzahl_verluste / (double)anzahl_gesamt;
 
-        // double[] resultat = new double[]{wahrsch_sieg, wahrsch_unentschieden, wahrsch_verlust};
-        double[] resultat = new double[]{anzahl_siege, anzahl_unentschieden, anzahl_verluste, anzahl_gesamt};
-        return resultat;
+        return new double[]{wahrsch_sieg, wahrsch_verlust, wahrsch_unentschieden};
     }
-
 }
